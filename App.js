d996e387e7d6e4ef3b97324291e3efbad643a278
Denis <asdw2001@gmail.com>
2023-02-16 16:01:29 +0600

    SetupComboBoxes

--- App.js
+++ App.js
@@ -10,11 +10,11 @@ import {
 import { Picker } from "@react-native-picker/picker";
 
 const years = [
-  { label: "2027", value: "2027" },
-  { label: "2026", value: "2026" },
-  { label: "2025", value: "2025" },
-  { label: "2024", value: "2024" },
   { label: "2023", value: "2023" },
+  { label: "2022", value: "2022" },
+  { label: "2021", value: "2021" },
+  { label: "2020", value: "2020" },
+  { label: "2019", value: "2019" },
 ];
 
 const semesters = [
@@ -29,31 +29,35 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.pickerContainer}>
-        <Picker
-          style={styles.picker}
-          mode="dropdown"
-          selectedValue={selectedYear}
-          onValueChange={(itemValue) => setSelectedYear(itemValue)}
-        >
-          {years.map((year, index) => (
-            <Picker.Item key={index} label={year.label} value={year.value} />
-          ))}
-        </Picker>
-        <Picker
-          style={styles.picker}
-          mode="dropdown"
-          selectedValue={selectedSemester}
-          onValueChange={(itemValue) => setSelectedSemester(itemValue)}
-        >
-          {semesters.map((semester, index) => (
-            <Picker.Item
-              key={index}
-              label={semester.label}
-              value={semester.value}
-            />
-          ))}
-        </Picker>
+      <View style={styles.pickers}>
+        <View style={styles.pickerContainer}>
+          <Picker
+            style={styles.picker}
+            mode="dropdown"
+            selectedValue={selectedYear}
+            onValueChange={(itemValue) => setSelectedYear(itemValue)}
+          >
+            {years.map((year, index) => (
+              <Picker.Item key={index} label={year.label} value={year.value} />
+            ))}
+          </Picker>
+        </View>
+        <View style={styles.pickerContainer}>
+          <Picker
+            style={styles.picker}
+            mode="dropdown"
+            selectedValue={selectedSemester}
+            onValueChange={(itemValue) => setSelectedSemester(itemValue)}
+          >
+            {semesters.map((semester, index) => (
+              <Picker.Item
+                key={index}
+                label={semester.label}
+                value={semester.value}
+              />
+            ))}
+          </Picker>
+        </View>
       </View>
       <View style={styles.inputContainer}>
         <Text style={styles.inputLabel}>Номер зачетки:</Text>
@@ -82,20 +86,22 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     fontSize: 16,
   },
+  pickers: {
+    flexDirection: "row",
+    paddingLeft: 15,
+  },
   pickerContainer: {
     borderColor: "#ccc",
     borderWidth: 1,
     borderRadius: 5,
-    paddingHorizontal: 10,
     marginBottom: 10,
-    flexDirection: "row",
+    marginRight: 15,
     alignItems: "center",
     justifyContent: "center",
   },
   picker: {
-    width: 125,
+    width: 126,
     height: 50,
-    marginRight: 10,
   },
   inputContainer: {
     borderColor: "#ccc",
