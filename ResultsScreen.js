212e2b40c8313ee4977a7fa3e4448a0aa6d50228
Denis <asdw2001@gmail.com>
2023-02-23 02:58:26 +0600

    Bud fixes

--- ResultsScreen.js
+++ ResultsScreen.js
@@ -1,14 +1,8 @@
 import React, { useState, useEffect } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  Button,
-  Dimensions,
-} from "react-native";
+import { StyleSheet, Text, View, Button, BackHandler } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
+import { Table, Row } from "react-native-table-component";
 
 export default function ResultsScreen() {
   const [results, setResults] = useState([]);
@@ -16,70 +10,89 @@ export default function ResultsScreen() {
   const [selectedYear, setSelectedYear] = useState("");
   const [selectedSemester, setSelectedSemester] = useState("");
   const navigation = useNavigation();
-  useEffect(() => {
-    async function getResults() {
-      try {
-        const storedResults = await AsyncStorage.getItem("results");
-        const storedStudentId = await AsyncStorage.getItem("studentId");
-        const storedSelectedYear = await AsyncStorage.getItem("selectedYear");
-        const storedSelectedSemester = await AsyncStorage.getItem(
-          "selectedSemester"
-        );
-        if (
-          storedResults !== null &&
-          storedStudentId !== null &&
-          storedSelectedYear !== null &&
-          storedSelectedSemester !== null
-        ) {
-          setResults(JSON.parse(storedResults));
-          setStudentId(storedStudentId);
-          setSelectedYear(storedSelectedYear);
-          setSelectedSemester(storedSelectedSemester);
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    }
-    getResults();
-  }, []);
 
   const handleGoBack = () => {
+    setResults([]);
     navigation.reset({
       index: 0,
       routes: [{ name: "Main" }],
     });
+    return true;
   };
 
-  const renderItem = ({ item }) => (
-    <View style={styles.result}>
-      <Text style={styles.subject}>{item.subject_name}</Text>
-      <Text style={styles.grade}>{item.grade}</Text>
-    </View>
-  );
+  async function getResults() {
+    try {
+      const storedResults = await AsyncStorage.getItem("results");
+      const storedStudentId = await AsyncStorage.getItem("studentId");
+      const storedSelectedYear = await AsyncStorage.getItem("selectedYear");
+      const storedSelectedSemester = await AsyncStorage.getItem(
+        "selectedSemester"
+      );
+
+      if (
+        storedResults !== null &&
+        storedStudentId !== null &&
+        storedSelectedYear !== null &&
+        storedSelectedSemester !== null
+      ) {
+        setResults(JSON.parse(storedResults));
+        setStudentId(storedStudentId);
+        setSelectedYear(storedSelectedYear);
+        setSelectedSemester(storedSelectedSemester);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  useEffect(() => {
+    getResults();
+    BackHandler.addEventListener("hardwareBackPress", handleGoBack);
+    return () => {
+      BackHandler.removeEventListener("hardwareBackPress", handleGoBack);
+    };
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Результаты экзаменов</Text>
-      <Text style={styles.subtitle}>Студент: {studentId}</Text>
-      <Text style={styles.subtitle}>Год: {selectedYear}</Text>
-      <Text style={styles.subtitle}>Семестр: {selectedSemester}</Text>
-      <FlatList
-        style={styles.results}
-        data={results}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.subject_name}
-      />
-      <Button title="Назад" onPress={handleGoBack} />
+      <View style={styles.header}>
+        <Text style={styles.title}>Результаты экзаменов</Text>
+        <Text style={styles.subtitle}>Студент: {studentId}</Text>
+        <Text style={styles.subtitle}>Год: {selectedYear}</Text>
+        <Text style={styles.subtitle}>Семестр: {selectedSemester}</Text>
+      </View>
+      <View style={styles.tableContainer}>
+        <Table style={styles.table}>
+          <Row
+            data={["Предмет", "Балл 1", "Балл 2", "Оценка"]}
+            style={styles.head}
+            textStyle={styles.textHead}
+          />
+          {results.map((result) => (
+            <Row
+              key={result.subject_name}
+              data={[
+                result.subject_name,
+                result.score_1k.toString(),
+                result.score_2k.toString(),
+                result.grade.toString(),
+              ]}
+              style={styles.row}
+              textStyle={styles.text}
+            />
+          ))}
+        </Table>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button style={styles.button} title="Назад" onPress={handleGoBack} />
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
     backgroundColor: "#f5fcff",
+    paddingTop: 80,
   },
   title: {
     fontSize: 20,
@@ -90,46 +103,44 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginVertical: 5,
   },
-  results: {
-    alignSelf: "stretch",
-    marginHorizontal: 20,
-    marginTop: 10,
-  },
-  result: {
-    flexDirection: "row",
-    marginVertical: 5,
-  },
-  subject: {
+  tableContainer: {
     flex: 1,
-    fontSize: 16,
+    marginHorizontal: 10,
+    marginBottom: 20,
   },
-  grade: {
+  table: {
+    flex: 1,
+  },
+  head: {
+    height: 40,
+    backgroundColor: "#f1f8ff",
+  },
+  textHead: {
     fontSize: 16,
     fontWeight: "bold",
+    textAlign: "center",
   },
-  center: {
-    justifyContent: "center",
+  row: {
+    height: 40,
+    flexDirection: "row",
     alignItems: "center",
+    backgroundColor: "#fff",
+    justifyContent: "space-between",
   },
-  resultsContainer: {
-    marginTop: 30,
-    marginBottom: 60,
-    ...Platform.select({
-      ios: {
-        maxHeight: Dimensions.get("window").height * 0.5,
-      },
-      android: {
-        maxHeight: Dimensions.get("window").height * 0.45,
-      },
-    }),
+  text: {
+    fontSize: 16,
+    textAlign: "center",
   },
-  emptyText: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginVertical: 20,
+  header: {
+    marginHorizontal: 20,
+    marginBottom: 10,
   },
-  backButton: {
-    position: "absolute",
-    bottom: 20,
+  buttonContainer: {
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  button: {
+    marginVertical: 10,
+    width: 150,
   },
 });
